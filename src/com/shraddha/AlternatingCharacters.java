6f0c33d4167399bcac32cb6c8571dbd99b75714d
Shraddha Shinde <shraddha.s@FinIQ-SP.com>
2021-10-25 12:25:32 +0530

    Alternating Characters code completed in java.

--- src/com/shraddha/AlternatingCharacters.java
+++ src/com/shraddha/AlternatingCharacters.java
@@ -1,7 +1,15 @@
 package com.shraddha;
 
-public class AlternatingCharachters {
+public class AlternatingCharacters {
+    public static int countAlternatingCharacters(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) if (str.charAt(i) == str.charAt(i + 1)) count++;
+        return count;
+    }
+
     public static void main(String[] args) {
-        String input = "AAAA";
+        String input = "BABB";
+        System.out.println("Input string = " + input);
+        System.out.println("Nubner of required deletions = " + countAlternatingCharacters(input));
     }
 }
